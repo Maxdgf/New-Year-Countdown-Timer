8f58ba98e2321cbd1740b45f8f4fd25d039b01ae
Max <162334963+Maxdgf@users.noreply.github.com>
2025-12-12 02:33:50 +0300

    Add dataclasses and code optimization

--- NewYearContdownTimerApp.py
+++ NewYearContdownTimerApp.py
@@ -9,11 +9,28 @@ Mini 🎄new year countdown timer web-app on bottle py.
 
 import json, calendar
 from datetime import datetime
+from dataclasses import dataclass
 from bottle import Bottle, template, response
 
 PATH_TO_INDEX = "templates/index.html" # path to html index
 PORT = 8000 # server port
 HOST = "localhost" # hostname
+
+@dataclass
+class TimeUntilNewYear:
+    hours: int
+    minutes: int
+    seconds: int
+
+@dataclass
+class StyleColorsheme:
+    primary_color: str
+    secondary_color: str
+
+@dataclass
+class DaysOfYearData:
+    days_passed_count: int
+    days_left_count: int
 
 class DatetimeManager:
     def __init__(self): pass
@@ -48,39 +65,37 @@ class DatetimeManager:
         # (primary color, secondary color)
         # -primary color = for page background mainly
         # -secondary color = for other elements(borders, buttons...)
-        if month_index >= 1 and month_index <= 2: return ("#03cffc", "#02a9cf") # winter colors
-        elif month_index >= 3 and month_index <= 5: return ("#03fca9", "#02cf8a") # spring colors
-        elif month_index >= 6 and month_index <= 8: return ("#6cff03", "#58d102") # summer colors
-        elif month_index >= 9 and month_index < 11: return ("#ff8903", "#db7704") # autumn colors
-        elif month_index == 12: return ("#73d5ff", "#67bce0") # december month colors
+        if month_index >= 1 and month_index <= 2: return StyleColorsheme(primary_color="#03cffc", secondary_color="#02a9cf") # winter colors
+        elif month_index >= 3 and month_index <= 5: return StyleColorsheme(primary_color="#03fca9", secondary_color="#02cf8a") # spring colors
+        elif month_index >= 6 and month_index <= 8: return StyleColorsheme(primary_color="#6cff03", secondary_color="#58d102") # summer colors
+        elif month_index >= 9 and month_index < 11: return StyleColorsheme(primary_color="#ff8903", secondary_color="#db7704") # autumn colors
+        elif month_index == 12: return StyleColorsheme(primary_color="#73d5ff", secondary_color="#67bce0") # december month colors
 
     # getting current month name
     def get_current_month_name(self): 
-        res_string = "" # output string
-
         datetime_now = datetime.now()
         month_index = datetime_now.month # current month index
+        month_name = datetime_now.strftime('%B')
         
         # adding to output string emoji by current month index
-        if month_index >= 1 and month_index <= 2: res_string += "❄️" # winter
-        elif month_index >= 3 and month_index <= 5: res_string += "🌸" # spring
-        elif month_index >= 6 and month_index <= 8: res_string += "☀️" # summer
-        elif month_index >= 9 and month_index < 11: res_string += "🍂" # autumn
-        elif month_index == 12: res_string += "🎄" # (special) december month
-
-        res_string += f"{datetime_now.strftime('%B')}" # adding formatted month name
-        
-        return res_string
+        if month_index >= 1 and month_index <= 2: return "❄️" + month_name # winter
+        elif month_index >= 3 and month_index <= 5: return "🌸" + month_name # spring
+        elif month_index >= 6 and month_index <= 8: return "☀️" + month_name # summer
+        elif month_index >= 9 and month_index < 11: return "🍂" + month_name # autumn
+        elif month_index == 12: return "🎄" + month_name # (special) december month
 
     # calculating days count until the new year and days passed count in the currrent year
-    def calculate_days_until_new_year(self): 
+    def calculate_days_data_until_new_year(self): 
         current_datetime = datetime.now()
         destination_datetime = datetime(current_datetime.year + 1, 1, 1, 0, 0, 0) # new year datetime
 
         days_left = (destination_datetime - current_datetime).days # days left to new year
         days_passed = self.get_days_count_in_year() - days_left # days passed in current year
 
-        return (days_passed, days_left)
+        return DaysOfYearData(
+            days_passed_count=days_passed, 
+            days_left_count=days_left
+        )
     
     # getting days count in current year
     def get_days_count_in_year(self): 
@@ -98,7 +113,11 @@ class DatetimeManager:
         hours = int(seconds / self.SECONDS_IN_HOUR) # hours left
         minutes = int(seconds / self.SECONDS_IN_MINUTE) # minutes left
 
-        return (hours, minutes, seconds)
+        return TimeUntilNewYear(
+            hours=hours,
+            minutes=minutes,
+            seconds=seconds
+        )
     
 app = Bottle() # bottle py object
 datetime_manager = DatetimeManager()
@@ -109,37 +128,40 @@ def index(): return template(PATH_TO_INDEX)
 @app.route("/api/current_datetime_now")
 def get_current_time():
     response.content_type = "application/json"
+    style_colorsheme = datetime_manager.get_colorsheme_of_current_time_of_year()
     # configuring current datetime data json
     return json.dumps(
         {
             "new_year": str(datetime_manager.get_new_year()),
             "datetime_now": f"{datetime_manager.get_time_now()}/{datetime_manager.get_date_now()}/{datetime_manager.get_current_month_name()}/{datetime_manager.get_day_of_week_now()}",
-            "first_hex_color": datetime_manager.get_colorsheme_of_current_time_of_year()[0],
-            "second_hex_color": datetime_manager.get_colorsheme_of_current_time_of_year()[1]
+            "first_hex_color": style_colorsheme.primary_color,
+            "second_hex_color": style_colorsheme.secondary_color
         }
     )
 
 @app.route("/api/countdown_timer_until_new_year_data")
 def get_countdown_timer_until_new_year_data():
     response.content_type = "application/json"
+    time_until_new_year = datetime_manager.get_time_data_until_new_year()
     # configuring countdown timer data json
     return json.dumps(
         {
-            "hours_left": datetime_manager.get_time_data_until_new_year()[0],
-            "minutes_left": datetime_manager.get_time_data_until_new_year()[1],
-            "seconds_left": datetime_manager.get_time_data_until_new_year()[2]
+            "hours_left": time_until_new_year.hours,
+            "minutes_left": time_until_new_year.minutes,
+            "seconds_left": time_until_new_year.seconds
         }
     )
 
 @app.route("/api/datetime_stats_until_new_year")
 def get_days_left_until_new_year():
     response.content_type = "application/json"
+    days_data = datetime_manager.calculate_days_data_until_new_year()
     # configuring datetime stats until new year data json
     return json.dumps(
         {
-            "days_in_year_and_days_passed_difference": f"{datetime_manager.calculate_days_until_new_year()[0]}/{datetime_manager.get_days_count_in_year()}",
-            "days_passed_in_current_year": datetime_manager.calculate_days_until_new_year()[0],
-            "days_left_until_new_year": str(datetime_manager.calculate_days_until_new_year()[1]),
+            "days_in_year_and_days_passed_difference": f"{days_data.days_passed_count}/{datetime_manager.get_days_count_in_year()}",
+            "days_passed_in_current_year": days_data.days_passed_count,
+            "days_left_until_new_year": str(days_data.days_left_count),
             "days_count_in_current_year": datetime_manager.get_days_count_in_year()
         }
     )
